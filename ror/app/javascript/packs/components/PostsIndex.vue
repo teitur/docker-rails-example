d7ba34621d316cf6589dcc7fd00f1d0133346191
teitur <teitur.arnarson@gmail.com>
2022-01-27 01:41:54 +0100

    Working VueJS example

---
+++ ror/app/javascript/packs/components/PostsIndex.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        Nota bene: {{ post.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostsIndex',
+  props: {
+    posts: {
+      type: Array,
+      default: () => [
+        {name: 'p1'}, 
+        {name: 'p2'}, 
+        {name: 'p3'}
+      ]
+    }
+  },
+  data: function () {
+    return {
+      message: "Stupid message"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
